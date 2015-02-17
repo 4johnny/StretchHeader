30e7c8ac4defda446f2ccef22e5131e48fe4f666
4johnny <4johnny@users.noreply.github.com>
2015-02-17 11:58:05 -0800

    Create master-detail view w/ custom table cell

---
+++ StretchHeader/StretchHeaderTableViewCell.h
@@ -0,0 +1,16 @@
+//
+//  StretchHeaderTableViewCell.h
+//  StretchHeader
+//
+//  Created by Johnny on 2015-02-17.
+//  Copyright (c) 2015 Empath Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface StretchHeaderTableViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *categoryLabel;
+@property (weak, nonatomic) IBOutlet UILabel *headlineLabel;
+
+@end
---
+++ StretchHeader/StretchHeaderTableViewCell.m
@@ -0,0 +1,26 @@
+//
+//  StretchHeaderTableViewCell.m
+//  StretchHeader
+//
+//  Created by Johnny on 2015-02-17.
+//  Copyright (c) 2015 Empath Solutions. All rights reserved.
+//
+
+#import "StretchHeaderTableViewCell.h"
+
+@implementation StretchHeaderTableViewCell
+
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+
+@end
